e4eea1383042bda4c032604c617aa3f8d9a75d5c
dmitry.bazyk <dmitry.bazyk@yandex.ru>
2026-01-31 18:35:14 +0400

    update App.tsx url path

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   const [items, setItems] = useState<any[]>([]);
 
   useEffect(() => {
-    const url = `/data/etsy.json`;
+    const url = `data/etsy.json`;
     fetch(url)
       .then((res) => {
         if (!res.ok) throw new Error(`HTTP ${res.status}`);
