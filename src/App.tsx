1483abc6263a2085b260258c5948059f8c68a993
dmitry.bazyk <dmitry.bazyk@yandex.ru>
2026-01-31 16:47:44 +0400

    fix bugs with vite.config.ts and base

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import Listing from "./components/Listing";
 
 const App = () => {
+  const [items, setItems] = useState<any[]>([]);
+
+  useEffect(() => {
+    const url = `/etsy.json`;
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) throw new Error(`HTTP ${res.status}`);
+        return res.json();
+      })
+      .then((data) => setItems(data))
+      .catch((err) => console.error("Ошибка загрузки:", err));
+  }, []);
+
   return (
     <div className="container">
-      <Listing />
+      <Listing items={items} />
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ra16-homeworks-props-listing/",
+  // base: "/ra16-homeworks-props-listing/",
 });
